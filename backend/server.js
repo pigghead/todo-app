cc519cf88d427d7e8fe0832da5a9e2e43c812893
Justin <justin.strongarms10@gmail.com>
2023-11-10 12:22:28 -0500

    package updates

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,16 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const taskRoutes = require('./routes/taskRoutes');
 
+dotenv.config();
+
+console.log('Before mongoose.connet: ', process.env.MONGO_URI);
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(taskRoutes);
-dotenv.config();
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.listen('8000', () => {
     console.log('listening on port 8000');
