6d6c55e356e2463f3f7bfbe17733f8bd02e9900f
Justin <justin.strongarms10@gmail.com>
2023-11-10 13:25:57 -0500

    added procfile

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require ('path');
 const taskRoutes = require('./routes/taskRoutes');
 
-dotenv.config();
-
-console.log('Before mongoose.connet: ', process.env.MONGO_URI);
+dotenv.config({path: path.join(__dirname, '..', '.env')});
+console.log(path.join(__dirname, '..', '.env'));
 
 const app = express();
 app.use(express.json());
@@ -14,6 +14,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(taskRoutes);
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get("/*", (req, res) => {
+    res.sendFile(__dirname, path.join(__dirname, '..', '.env'))
+    console.log(__dirname, path.join(__dirname, '..', '/frontend/index.js'));
+});
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
