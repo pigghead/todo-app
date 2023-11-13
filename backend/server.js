e5c42551ca3d091ceda72d20ca35b396cce379f4
Justin <justin.strongarms10@gmail.com>
2023-11-13 15:27:50 -0500

    serve static differently

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
-app.use(express.static('/', express.static(path.join(__dirname, '../frontend/build'))));
-app.get("/*", (req, res) => {
+//app.use(express.static('/', express.static(path.join(__dirname, '../frontend/build'))));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(taskRoutes);
+app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "frontend", "public", "index.html"));
     console.log(path.resolve(__dirname, "..", "frontend", "public", "index.html"));
 });
-app.use(taskRoutes);
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const PORT = process.env.PORT || 5001
