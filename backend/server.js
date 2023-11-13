f29958939c0ff3271ccad587bb6e33275647bf95
Justin <justin.strongarms10@gmail.com>
2023-11-13 14:35:53 -0500

    readded sendFile

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(express.static('/static', express.static(path.join(__dirname, '../frontend/build'))));
 app.get("/", (req, res) => {
-    //res.sendFile(path.resolve(__dirname, "..", "frontend", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "frontend", "public", "index.html"));
     console.log("Hello :root")
     //console.log(path.resolve(__dirname, "frontend", "public", "index.html"));
 });
