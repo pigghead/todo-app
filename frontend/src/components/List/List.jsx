4bf339180f2346bc4bfcc369998125521d67f9f1
Justin <justin.strongarms10@gmail.com>
2023-11-13 11:28:46 -0500

    update to package.json postbuild script

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -27,8 +27,8 @@ export const List = () => {
 
         try {
             const newTask = {completed:false, title, desc};
-            setTasks([...tasks, newTask]);
             await axios.post(`${URL}/tasks`, taskData);
+            setTasks(prevTasks => [...prevTasks, newTask]);
             // Reset form data to blank
             setTaskData({title: "", desc: ""});
             getTasks();
@@ -50,6 +50,7 @@ export const List = () => {
         try {
             const {data} = await axios.get(`${URL}/tasks`);
             setTasks(data);
+            console.log(tasks.length);
             setIsLoading(false);
         } catch (err) {
             alert(err.message);
@@ -120,7 +121,7 @@ export const List = () => {
                     <p>Add a task to get started</p>
                 ) : (
                     <>
-                    {tasks && tasks.length && tasks.map((task, index) => {
+                    {tasks && tasks.length > 0 && tasks.map((task, index) => {
                         return (
                             <Task 
                                 key={task._id}
